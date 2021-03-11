c08cf8247e8042563ee743c42171d3cd3aea466a
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-03-11 21:42:00 +0100

    start oblig 1

---
+++ oblig1/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oblig1
+{
+    public class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public int BirthYear;
+        public int DeathYear;
+        public Person Father;
+        public Person Mother;
+        public string GetDescription()
+        {
+            string description = "";
+            if (FirstName != null)
+            {
+                description += FirstName + " ";
+            }
+            if (LastName != null)
+            {
+                description += LastName + " ";
+            }
+            if (Id != 0)
+            {
+                description += "(Id=" + Id + ") ";
+            }
+            if (BirthYear != 0)
+            {
+                description += "Født: " + BirthYear;
+            } 
+            if (DeathYear != 0)
+            {
+                description += "Død: " + DeathYear;
+            }
+            if (Father != null)
+            {
+                if (Father.FirstName != null)
+                {
+                    description += "Far: " + Father.FirstName + " ";
+                }
+                if (Father.Id != 0)
+                {
+                    description += "(Id=" + Father.Id + ") ";
+                }
+            }
+            if (Mother != null)
+            {
+                if (Mother.FirstName != null)
+                {
+                    description += "Mor: " + Mother.FirstName + " ";
+                }
+                if (Mother.Id != 0)
+                {
+                    description += "(Id=" + Mother.Id + ")";
+                }
+            }
+            return FirstName + " " + LastName + " " + "(Id=" + Id + ") " + "Født: " + BirthYear
+            + " " + "Død: " + DeathYear + " " + "Far: " + Father.FirstName + " " + "(Id=" + Father.Id + ") " + "Mor: " + Mother.FirstName + " " + "(Id=" + Mother.Id + ")";
+            
+        }
+    }
+}
---
+++ oblig1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace oblig1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person
+            {
+                Id = 17,
+                FirstName = "Ola",
+                LastName = "Nordmann",
+                BirthYear = 2000,
+                DeathYear = 3000,
+                Father = new Person() { Id = 23, FirstName = "Per" },
+                Mother = new Person() { Id = 29, FirstName = "Lise" },
+            };
+            Console.WriteLine(p.GetDescription());
+        }
+    }
+}
