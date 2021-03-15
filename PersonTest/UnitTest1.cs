23f8ad218d0369450d2d9b173927529c4a1544db
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-03-15 13:20:12 +0100

    yiss

--- PersonTest/UnitTest1.cs
+++ PersonTest/UnitTest1.cs
@@ -24,5 +24,37 @@ namespace PersonTest
 
             Assert.AreEqual(expectedDescription, actualDescription);
         }
+
+        [Test]
+        public void TestNoFields()
+        {
+            var p = new Person
+            {
+                Id = 1,
+            };
+
+            var actualDescription = p.GetDescription();
+            var expectedDescription = "(Id=1)";
+
+            Assert.AreEqual(expectedDescription, actualDescription);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var p2 = new Person
+            {
+                Id = 17,
+                FirstName = "Ola",
+                BirthYear = 2000,
+                Father = new Person() {FirstName = "Per" },
+                Mother = new Person() { Id = 29, FirstName = "Lise" },
+            };
+
+            var actualDescription = p2.GetDescription();
+            var expectedDescription = "Ola (Id=17) Født: 2000 Far: Per Mor: Lise (Id=29)";
+
+            Assert.AreEqual(expectedDescription, actualDescription);
+        }
     }
 }
--- oblig1/Person.cs
+++ oblig1/Person.cs
@@ -16,51 +16,66 @@ namespace oblig1
         public string GetDescription()
         {
             string description = "";
-            if (FirstName != null)
+
+            if (FirstName == null && LastName == null && BirthYear == 0 && DeathYear == 0 && Father == null && Mother == null)
+            {
+                return description = string.Format("(Id={0})", Id);
+            }
+
+            
+            else if (FirstName != null)
             {
                 description += FirstName + " ";
             }
-            if (LastName != null)
-            {
-                description += LastName + " ";
-            }
-            if (Id != 0)
-            {
-                description += "(Id=" + Id + ") ";
-            }
-            if (BirthYear != 0)
-            {
-                description += "Født: " + BirthYear;
-            } 
-            if (DeathYear != 0)
-            {
-                description += "Død: " + DeathYear;
-            }
-            if (Father != null)
-            {
-                if (Father.FirstName != null)
+                if (LastName != null)
                 {
-                    description += "Far: " + Father.FirstName + " ";
+                    description += LastName + " ";
                 }
-                if (Father.Id != 0)
+                if (Id != 0)
                 {
-                    description += "(Id=" + Father.Id + ") ";
+                    description += "(Id=" + Id + ")";
                 }
-            }
-            if (Mother != null)
-            {
-                if (Mother.FirstName != null)
+                if (BirthYear != 0)
                 {
-                    description += "Mor: " + Mother.FirstName + " ";
-                }
-                if (Mother.Id != 0)
+                    description += " " + "Født: " + BirthYear + " ";
+                } 
+                if (DeathYear != 0)
                 {
-                    description += "(Id=" + Mother.Id + ")";
+                    description += "Død: " + DeathYear + " ";
                 }
-            }
-            return FirstName + " " + LastName + " " + "(Id=" + Id + ") " + "Født: " + BirthYear
-            + " " + "Død: " + DeathYear + " " + "Far: " + Father.FirstName + " " + "(Id=" + Father.Id + ") " + "Mor: " + Mother.FirstName + " " + "(Id=" + Mother.Id + ")";
+                if (Father != null)
+                {
+                    if (Father.FirstName != null)
+                    {
+                        description += "Far: " + Father.FirstName + " ";
+                    }
+                    if (Father.Id != 0)
+                    {
+                        description += "(Id=" + Father.Id + ") ";
+                    }
+                }
+                if (Mother != null)
+                {
+                    if (Mother.FirstName != null)
+                    {
+                        description += "Mor: " + Mother.FirstName + " ";
+                    }
+                    if (Mother.Id != 0)
+                    {
+                        description += "(Id=" + Mother.Id + ")";
+                    }
+                }
+            //    else
+            //{
+            //    description += string.Format("{0} {1} (Id={2}) Født: {3} Død: {4} Far: {5} (Id={6}) Mor: {7} (Id={8}",
+            //        FirstName, LastName, Id, BirthYear, DeathYear, Father.FirstName, Father.Id, Mother.FirstName, Mother.Id);
+            //}
+
+              return description;
+
             
         }
+
+       
     }
 }
